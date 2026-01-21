ad26dc8434ff9a94846d5c210e331b619c46e21b
Kristian Serafin <kristiandeguzman09@gmail.com>
2026-01-22 02:24:05 +0800

    changed 404 image
    /img/coming-soon.png

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,8 @@
 @section("main")
     <div class="flex w-full flex-col items-center justify-center gap-4 h-full">
         <div class="flex flex-col gap-2 text-center">
-            <img src="/img/404.jpg" />
+            <!-- <img src="/img/404.jpg" /> -->
+            <img src="/img/coming-soon.png" class="w-full max-w-3xl md:max-w-4xl h-auto" />
         </div>
     </div>
 @endsection
